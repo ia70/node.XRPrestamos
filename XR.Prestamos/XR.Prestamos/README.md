377cf61371fb76a2b829c2b7012ad29608d069d2
xcoru <pcd510@hotmail.com>
2019-10-09 14:38:36 -0500

    Cambios

--- XR.Prestamos/XR.Prestamos/README.md
+++
@@ -1,3 +0,0 @@
-﻿# XR.Prestamos
-
-
---
+++ XRPrestamos/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Settings
+
+// Middlewares
+
+// Routes
+
+// Static files
+
+
+app.listen(300, ()=> {
+    console.log('server on port 3000');
+})
