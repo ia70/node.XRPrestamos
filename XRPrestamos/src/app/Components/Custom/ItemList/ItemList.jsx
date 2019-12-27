3a62c0adf644555411990584a9d5c9dd35a9272d
xcoru <pcd510@hotmail.com>
2019-12-27 12:40:07 -0600

    Cambios

--- XRPrestamos/src/app/Components/Custom/ItemList/ItemList.jsx
+++ XRPrestamos/src/app/Components/Custom/ItemList/ItemList.jsx
@@ -50,12 +50,12 @@ export function ItemList(props) {
         <div className="col-xs-12 col-sm-6 col-lg-4 col-xl-3 mb-3 stateitem">
             <div className={"container-fluid stateitem_border_br stateitem_border_bl stateitem_border_tr stateitem_border_tl stateitem_state_" + props.stateItem}>
                 <div className="row">
-                    <div className={"col-auto stateitem_num stateitem_center stateitem_border_tl " + class_bl}>
+                    <div className={"col-xs-12 col-auto stateitem_num stateitem_center stateitem_border_tl " + class_bl}>
                         <h2>{props.number}</h2>
                     </div>
-                    <div className="col">
+                    <div className="col col-xs-12">
                         <div className="row stateitem_alias stateitem_center stateitem_border_tr">
-                            <div className="col stateitem_center">
+                            <div className="col col-xs-12 stateitem_center">
                                 {props.alias}
                             </div>
                             {btncerrar}
@@ -63,11 +63,11 @@ export function ItemList(props) {
                         <div className="row stateitem_name stateitem_center">
                             {props.name}
                         </div>
-                        <div className={"row stateitem_center " + class_br}>
-                            <div className="col stateitem_state_amount_description">
+                        <div className={"row align-self-stretch stateitem_center " + class_br}>
+                            <div className="col col-xs-12 stateitem_state_amount_description">
                                 {props.amountDescription}
                             </div>
-                            <div className={"col-auto stateitem_state_amount " + class_br}>
+                            <div className={"col-auto col-xs-12 align-self-stretch align-items-center stateitem_state_amount " + class_br}>
                                 {"$" + props.amount}
                             </div>
                         </div>
