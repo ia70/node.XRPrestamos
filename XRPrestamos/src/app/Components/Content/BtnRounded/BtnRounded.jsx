a63257fd8414d252d5c1c3128df4a1063126bc41
Daniel Perez <pcd510@hotmail.com>
2019-12-28 20:16:25 -0600

    Cambios

--- XRPrestamos/src/app/Components/Content/BtnRounded/BtnRounded.jsx
+++ XRPrestamos/src/app/Components/Content/BtnRounded/BtnRounded.jsx
@@ -5,9 +5,15 @@ import './BtnRounded.css';
 class BtnRounded extends Component {
     render() {
         return (
-            <div className="Cont m-1 p-1 btn btn-outline-info" onClick={() => this.props.event(this.props.id)} >
-                <img src={this.props.src} className="border border-warning Cont2 rounded-circle" />
-                <strong className="text-center Cont2" >{this.props.texto}</strong>
+            <div className="col m-1 p-1 btn btn-outline-info" onClick={() => this.props.event(this.props.id)} >
+                <div className="row">
+                    <div className="col-12">
+                        <img src={this.props.src} className="border border-warning rounded-circle" />
+                    </div>
+                    <div className="col-12">
+                        <strong className="text-center" >{this.props.texto}</strong>
+                    </div>
+                </div>
             </div>
         );
     }
