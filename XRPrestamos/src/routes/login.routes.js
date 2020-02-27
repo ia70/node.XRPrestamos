5f2d731d6b463df049501adaf8d2e650fc71e335
Daniel Perez <pcd510@hotmail.com>
2020-02-26 22:43:36 -0600

    Cambios

--- XRPrestamos/src/routes/login.routes.js
+++ XRPrestamos/src/routes/login.routes.js
@@ -10,13 +10,18 @@ const primary_key = "id_usuario";
 router.get('/', async (req, res) => {
     try {
         const data = await pool.query('SELECT * FROM ' + tabla + ' WHERE id_usuario="' + req.body.id_usuario + '" AND password="' + req.body.password + '"');
-        console.log(data);
-        res.status(200).send({
-          login: true,
-          datos: data
-        });
+
+        if (JSON.stringify(data) == '[]') {
+            res.status(400).send({
+                login: false,
+            });
+        } else {
+            res.status(200).send({
+                login: true,
+            });
+        }
     } catch (e) {
-        res.status(400).send(e);
+        res.status(400).send({login: false});
     }
 });
 
