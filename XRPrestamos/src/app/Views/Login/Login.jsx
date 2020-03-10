b2ec2277cc04703b3932c2ec8cf7f4905140c838
Daniel Perez <pcd510@hotmail.com>
2020-03-09 19:02:10 -0600

    Cambios

--- XRPrestamos/src/app/Views/Login/Login.jsx
+++ XRPrestamos/src/app/Views/Login/Login.jsx
@@ -9,7 +9,7 @@ import { BtnSubmit } from '../../Components/Form/BtnSubmit/BtnSubmit.jsx';
 import './Login.css';
 
 const keys = require('../../../keys');
-require('../../../cipher');
+const enc = require('../../../cipher');
 
 
 //PROPIEDADES ------------------------------
@@ -32,8 +32,8 @@ class Login extends Component {
         var usuario = document.getElementById('txtusuario').value;
         var password = document.getElementById('txtpassword').value;
 
-        var e_user = encode(keys.security.client_password.toString, usuario);
-        var e_pass = encode(keys.security.client_password.toString, password);
+        var e_user = enc.encode(keys.security.client_password.toString, usuario);
+        var e_pass = enc.encode(keys.security.client_password.toString, password);
 
         var url = 'http://' + keys.database.host + '/api/login?usr=' + e_user + '&pwd=' + e_pass;
 
