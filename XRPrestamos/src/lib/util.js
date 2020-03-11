8732be0b072440e073e69d4dbdc2cfc17e4de572
Daniel Perez <pcd510@hotmail.com>
2020-03-10 22:04:24 -0600

    Cambios

--- XRPrestamos/src/lib/util.js
+++ XRPrestamos/src/lib/util.js
@@ -3,7 +3,8 @@ import { moment } from 'moment';
 function getDateTime() {
     var d = new Date();
     var fecha = d.getFullYear() + '-' + d.getDate() + '-' + d.getDay() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-    return moment(fecha).format('YYYY-MM-DD HH:MM:SS');
+    var fe = moment(fecha, 'YYYY-MM-DD hh:mm:ss a');
+    return fe;
 }
 
 module.exports = { getDateTime };
