784a140105d3d68024fd6a293ce06546208bdc1d
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-03-23 18:00:29 -0600

    Cambios

--- XRPrestamos/src/routes/lib/estado_solicitud.routes.js
+++ XRPrestamos/src/routes/lib/estado_solicitud.routes.js
@@ -9,17 +9,17 @@ const { access } = require('../../lib/security');
 router.post('/', async (req, res) => {
     try {
         if (await access(req.body.hash, req.body.user)) {
-            let r_d_cartera = false;
+            let r_d_solicitud = false;
 
-            const d_car = await pool.query('CALL lista_solicitudes(?)', [req.body.user]);
+            const d_sol = await pool.query('CALL lista_solicitudes(?)', [req.body.user]);
 
-            if (JSON.stringify(d_car) != '[]') {
-                r_d_cartera = true;
+            if (JSON.stringify(d_sol) != '[]') {
+                r_d_solicitud = true;
             }
             let respuesta = {
-                response: r_d_cartera,
+                response: r_d_solicitud,
                 session: true,
-                cartera: d_car[0]
+                solicitud: d_sol[0]
             };
 
             res.status(200).send(respuesta);
