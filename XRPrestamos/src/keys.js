bdbabf2bf2aa257f250c5d38f87afe2c0fb7effe
xcoru <pcd510@hotmail.com>
2020-06-18 18:35:40 -0500

    configuraciones

--- XRPrestamos/src/keys.js
+++ XRPrestamos/src/keys.js
@@ -1,21 +1,21 @@
 'use strict';
 module.exports = {
     database: {
-        host: '192.168.184.55',
+        host: '192.168.1.50',
         user: 'xcoru',
         password: 'xcoru12345',
         database: 'xrprestamos'
     },
     server: {
-        host: '192.168.184.50',
+        host: '192.168.1.65',
         port: 80
     },
-    security:{
+    security: {
         main_password: '@as_T69?1pq-XoAsqlfpZ_@rgsfdlwoX',
         client_password: 'dO_@-tpA59z-klwquf02_gofldj_2-3R',
         iv_password: '@_aTfdh%Mkxd-k_Q'     //Exactamente 16 caracteres
     },
-    api:{
-        url: 'http://192.168.184.55/api/'
+    api: {
+        url: 'http://' + this.database.host + '/api/'
     }
 };
