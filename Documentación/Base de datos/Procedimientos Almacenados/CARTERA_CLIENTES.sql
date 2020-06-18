9cbccafed37356ce6804f6e47501229726fcc3dc
xcoru <pcd510@hotmail.com>
2020-06-18 17:09:58 -0500

    Corrección PROCEDIMIENTO CARTERA CLIENTES

--- Documentación/Base de datos/Procedimientos Almacenados/CARTERA_CLIENTES.sql
+++ Documentación/Base de datos/Procedimientos Almacenados/CARTERA_CLIENTES.sql
@@ -2,14 +2,20 @@ DROP PROCEDURE IF EXISTS CARTERA_CLIENTES;
  DELIMITER //
 CREATE PROCEDURE CARTERA_CLIENTES(IN usuario VARCHAR(30))
    BEGIN
-		SELECT e.alias, e.nombre, e.apellido_paterno, e.apellido_materno, f.monto_total, SUM(g.monto) AS "pagado", (f.monto_total - SUM(g.monto)) AS "restante" FROM usuario a
-		INNER JOIN usuario_ruta AS b ON b.id_usuario = a.id_usuario
-		INNER JOIN ruta AS c ON c.id_ruta = b.id_ruta
-		INNER JOIN usuario_establecimiento AS d ON d.id_ruta = c.id_ruta
-		INNER JOIN persona AS e ON e.ine = d.ine
-		RIGHT JOIN credito AS f ON f.ine = e.ine
-		LEFT JOIN abono AS g ON g.folio_credito = f.folio_credito
+		-- a Persona
+		-- b Credito
+		-- c Abono
+		-- d Usuario_establecimiento
+		-- e Usuario_ruta
+		-- f Usuario
 
-		WHERE a.id_usuario = usuario GROUP BY f.folio_credito;
+		SELECT a.alias, a.nombre, a.apellido_paterno, a.apellido_materno, b.monto_total, SUM(c.monto) AS "pagado", (b.monto_total - SUM(c.monto)) AS "restante" FROM credito b
+			INNER JOIN usuario_establecimiento AS d ON d.id_usuario_establecimiento = b.id_usuario_establecimiento
+			INNER JOIN persona AS a ON a.ine = d.ine
+			INNER JOIN usuario_ruta AS e ON e.id_ruta = d.id_ruta
+			INNER JOIN usuario AS f ON f.id_usuario = e.id_usuario
+			JOIN abono AS c 
+			WHERE f.id_usuario = usuario AND b.id_estado_credito = 1
+			GROUP BY b.folio_credito;
    END //
 DELIMITER ;
