8ce3255662b2a48a326dff534ca134fb6c4fdfa3
xcoru <pcd510@hotmail.com>
2020-06-24 00:20:24 -0500

    Update ModalCollect.jsx

--- XRPrestamos/src/app/Components/Custom/ModalCollect/ModalCollect.jsx
+++ XRPrestamos/src/app/Components/Custom/ModalCollect/ModalCollect.jsx
@@ -35,50 +35,38 @@ export function ModalCollect(props) {
                         <div className="row modalcollect_name m-0 align-items-center">
                             <h4 className="tm2">{props.name}</h4>
                         </div>
-                        
+
+
                         <div className="table-responsive">
                             <table className="table">
                                 <thead className="thead-dark">
                                     <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
-                                        <th scope="col">Handle</th>
-                                        <th scope="col">Handle</th>
-                                        <th scope="col">Handle</th>
+                                        <th scope="col">Conc.</th>
+                                        <th scope="col">Pagos</th>
+                                        <th scope="col">$ Monto</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <th scope="row">1</th>
-                                        <td>Daniel Pérez Cortez</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr className="table-success">
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
+                                        <th scope="row">Atrasos</th>
+                                        <td>3</td>
+                                        <td>1500</td>
                                     </tr>
                                     <tr>
-                                        <th scope="row">3</th>
-                                        <td colSpan="2">Larry the Bird</td>
-                                        <td>@twitter</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
+                                        <th scope="row">Adelantos</th>
+                                        <td>0</td>
+                                        <td>0</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Restante</th>
+                                        <td>7</td>
+                                        <td>3000</td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
+
+
 
                         <div className="row modalcollect_monto m-0 py-0 my-2">
                             Pago del día: ${props.amount}
@@ -87,36 +75,22 @@ export function ModalCollect(props) {
                         <div className="row modalcollect_body m-0 p-1">
 
                             <div className="input-group bg-warning mx-3 my-1 p-1">
-                                <div className="input-group-prepend">
-                                    <div className="input-group-text">
-                                        <input id={'txtabono2' + props.number} type="radio" name="exampleRadios" aria-label="Radio button for following text input" />
-                                    </div>
-                                </div>
-                                <input id={'txtabono' + props.number} type="number" className="form-control" min={0} placeholder="Abono" aria-label="Text input with radio button" />
+                                <input id={'txtmonto' + props.number} type="number" className="form-control" min={0} placeholder="Pago recibido:" aria-label="Text input with radio button" />
                             </div>
 
                             <div className="w-100"></div>
-
-                            <div className="col my-3">
-                                <div className="form-check bg-success text-white py-2 redondear">
-                                    <input id={'txtpagocompleto' + props.number} className="form-check-input" type="radio" name="exampleRadios" value="1" />
-                                    <label className="form-check-label" htmlFor={'txtpagocompleto' + props.number}>
-                                        Pagó completo
-                                    </label>
-                                </div>
-                            </div>
-                            <div className="col">
-                                <div className="form-check bg-danger text-white py-2 redondear">
-                                    <input id={'txtnopago' + props.number} className="form-check-input" type="radio" name="exampleRadios" value="2" />
-                                    <label className="form-check-label" htmlFor={'txtnopago' + props.number}>
-                                        No pagó
-                                    </label>
-                                </div>
-                            </div>
                         </div>
                     </div>
                     <div className="modal-footer px-3 mx-0">
-                        <button type="button" className="btn btn-success col-12 text-white">Guardar</button>
+                        <div className="col-2">
+                            <button type="button" className="btn btn-info col-12 text-white">Info.</button>
+                        </div>
+                        <div className="col">
+                            <button type="button" className="btn btn-danger col-12 text-white">No pagó</button>
+                        </div>
+                        <div className="col">
+                            <button type="button" className="btn btn-success col-12 text-white">Guardar pago</button>
+                        </div>
                     </div>
                 </div>
             </div>
