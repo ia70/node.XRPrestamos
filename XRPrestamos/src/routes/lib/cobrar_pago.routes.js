f8029db6fa1a54548ace1346dbfd548315ddaa1c
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-06-27 17:53:40 -0500

    Create cobrar_pago.routes.js

---
+++ XRPrestamos/src/routes/lib/cobrar_pago.routes.js
@@ -0,0 +1,45 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const { access } = require('../../lib/security');
+
+const pool = require('../../database');
+
+//->>>>>    AGREGAR     --------------------------------------------------------------------
+router.post('/', async (req, res) => {
+    try {
+        var solicitud = req.body.solicitud;
+        var user = req.body.user;
+        var db_res = false;
+
+        if (await access(user.hash, user.user)) {
+
+
+            const query = await pool.query('SELECT * FROM abono');
+            //const query = await pool.query('INSERT INTO abono SET ?', [solicitud]);
+            if (query.affectedRows > 0) {
+                db_res = true;
+            }
+
+            let respuesta = {
+                response: db_res,
+                session: true
+            };
+
+            res.status(200).send(respuesta);
+
+        } else {
+            res.status(400).send({
+                session: false
+            });
+        }
+    } catch (e) {
+        res.status(400).send({
+            response: false,
+            session: true,
+            error: e
+        });
+    }
+});
+
+module.exports = router;
