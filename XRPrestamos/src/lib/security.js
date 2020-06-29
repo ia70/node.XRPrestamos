314e8a0f48c42e41a7882198bbbb347e8176fa26
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-06-28 23:44:50 -0500

    Update security.js

--- XRPrestamos/src/lib/security.js
+++ XRPrestamos/src/lib/security.js
@@ -4,14 +4,12 @@ const { getDateTime } = require('../lib/util');
 async function access(vhash, vuser) {
     try {
         let data = await pool.query('UPDATE sesion SET ultima_conexion="' + getDateTime() + '" WHERE id_sesion="' + vhash + '" AND id_usuario="' + vuser + '" AND id_estado=1');
-        //console.log(data);
         if (data.affectedRows == 0) {
             return false;
         } else {
             return true;
         }
     } catch (error) {
-        //console.log(error);
         return false;
     }
 
