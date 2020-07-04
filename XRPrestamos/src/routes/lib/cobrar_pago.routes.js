b22dafffaf4ddf1efd17e4013447a86761691873
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-07-03 20:05:07 -0500

    Update cobrar_pago.routes.js

--- XRPrestamos/src/routes/lib/cobrar_pago.routes.js
+++ XRPrestamos/src/routes/lib/cobrar_pago.routes.js
@@ -15,78 +15,96 @@ router.post('/', async (req, res) => {
 
         if (await access(user.hash, user.user)) {
 
-            // CALCULOS DE INFORMACION DE ABONO
-
-            /*
-                1 - Carcular campos de abono
-                2 - Insertar/Modificar abono 
-                3 - Insertar/Modificar estadistica
-                4 - Modificar credito
-
-                solicitud: {
-                    folio_credito: this.state.info_credito.folio_credito,
-                    monto: _monto,
-                    no_pagos: 0,
-                    id_tipo_pago: 0,
-                    fecha_abono: Fecha.getShortDate(),
-                    id_estado: 1,
-                    fecha_reg: Fecha.getDateTime()
-                },
-            */
-
-            // CALCULAR CAMPOS ABONO E INSERTAR
-            if (solicitud.monto > info.monto_pago) {
+            // CALCULAR CAMPOS ABONO --------------------------------------------------------------------------------------------------------------
+            if (info.restante_total <= solicitud.monto) {
+                solicitud.id_tipo_pago = 6;
+                solicitud.no_pagos = solicitud.monto / info.monto_pago;
+                // Modificar tabla credito
+                let db_credito = await pool.query('UPDATE credito SET estado_credito=5, estado=2 WHERE folio_credito = ?', [info.folio_credito]);
+            } else if (solicitud.monto > info.monto_pago) {
                 solicitud.id_tipo_pago = 3;
-                solicitud.no_pagos = Math.trunc(solicitud.monto / info.monto_pago);
+                solicitud.no_pagos = solicitud.monto / info.monto_pago;
+            } else if (solicitud.monto == info.monto_pago) {
+                solicitud.id_tipo_pago = 1;
+                solicitud.no_pagos = 1;
             } else if (solicitud.monto > 0) {
                 solicitud.id_tipo_pago = 2;
-                solicitud.no_pagos = 1;
+                solicitud.no_pagos = solicitud.monto / info.monto_pago;
             } else {
                 solicitud.id_tipo_pago = 4;
-                solicitud.no_pagos = 1;
+                solicitud.no_pagos = 0;
             }
 
-            //const queryAbono = await pool.query('INSERT INTO abono SET ?', [solicitud]);
+            let queryAbono = await pool.query('INSERT INTO abono SET ?', [solicitud]);
 
 
-            let estadistica = await pool.query('SELECT * FROM estadistica_cliente WHERE folio_credito = ?', solicitud.folio_credito);
-
-            if(estadistica.values.length == 0){
-                console.log("ENTRO");
+            // CALCULO DE ESTADISTICAS -------------------------------------------------------------------------------------------------------------
+            let estadistica = await pool.query('SELECT * FROM estadistica_cliente WHERE folio_credito = ?', [solicitud.folio_credito]);
+            let estadistica_existe = true;
+            
+            if (estadistica == "") {
+                estadistica_existe = false;
                 estadistica = {
-                    folio_credito : info.folio_credito,
-                    ine : info.ine,
-                    monto_credito : info.monto_credito,
-                    fecha_entrega : info.fecha_entrega,
-                    pagos_total : info.pagos_total,
-                    no_pagos : 0,
-                    no_abonos : 0,
-                    no_adelantos : 0,
-                    no_atrasos : 0
+                    folio_credito: info.folio_credito,
+                    ine: info.ine,
+                    monto_credito: info.monto_credito,
+                    fecha_entrega: info.fecha_entrega,
+                    pagos_total: info.pagos_total,
+                    no_pagos: info.pagos_total - (info.restante_no - 1),
+                    no_abonos: (solicitud.id_tipo_pago == 2 ? 1 : 0),
+                    no_adelantos: (solicitud.id_pago == 3 ? info.extras_no + 1 : info.extras_no),
+                    no_atrasos: (solicitud.id_tipo_pago == 4 ? info.atrasos_no + 1 : (solicitud.id_tipo_pago == 2 ? solicitud.monto / info.monto_pago : 0))
                 };
+            } else {
+                // NO_PAGOS
+                estadistica.no_pagos += info.pagos_total - (info.restante_no + 1);
+
+                // NO_ABONOS
+                if (solicitud.id_tipo_pago == 2) {
+                    estadistica.no_abonos += 1;
+                }
+
+                // NO_ATRASOS
+                if (solicitud.id_tipo_pago == 4) {
+                    estadistica.no_atrasos += 1;
+                } else if (solicitud.id_tipo_pago == 2) {
+                    estadistica.no_atrasos += (solicitud.monto / info.monto_pago);
+                }
+
+                // NO_ADELANTOS
+                if (solicitud.id_tipo_pago == 3) {
+                    let cal_extra = info.monto_pago - solicitud.monto;
+                    if ((info.extras_monto + cal_extra) > info.atrasos_monto) {
+                        estadistica.no_adelantos += cal_extra / info.monto_pago;
+                    }
+                }
             }
 
-            console.log(estadistica);
 
-            //const queryConsultaEstadistica = await pool.query('UPDATE estadistica_cliente SET ? WHERE folio_credito = ?', [, req.body[primary_key]]);
-            //const queryConsultaEstadistica = await pool.query('SELECT * FROM estadistica_cliente WHERE folio_credito = ?', "11");
-            //console.log(queryConsultaEstadistica);
-            //if(queryConsultaEstadistica.)
 
-/*
+            // INSERTAR O ACTUALIZAR BASE DE DATOS DE ESTADISTICA
+            if (estadistica_existe) {
+                let queryEstadistica = await pool.query('UPDATE estadistica_cliente SET ? WHERE folio_credito = ?', [estadistica[0], info.folio_credito]);
+            } else {
+                let queryEstadistica = await pool.query('INSERT INTO estadistica_cliente SET ?', [estadistica]);
+            }
 
-            const query = await pool.query('CALL COBRO_DIA_CONSULTA(?)', solicitud.folio_credito);
-            //const query = await pool.query('INSERT INTO abono SET ?', [solicitud]);
-            if (query.affectedRows > 0) {
+
+            // FIN CALCULO ESTADISTICA -------------------------------------------------------------------------------------------------------------
+
+
+            let queryDataAbono = await pool.query('CALL COBRO_DIA_CONSULTA(?)', solicitud.folio_credito);
+
+            if (queryAbono.affectedRows > 0) {
                 db_res = true;
             }
 
             let respuesta = {
                 response: db_res,
-                data: query,
+                data: queryDataAbono[0],
                 session: true
             };
-*/
+
             res.status(200).send(respuesta);
 
         } else {
@@ -95,6 +113,7 @@ router.post('/', async (req, res) => {
             });
         }
     } catch (e) {
+        console.log(e);
         res.status(400).send({
             response: false,
             session: true,
