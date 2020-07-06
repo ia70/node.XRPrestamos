605438e020829514fa1636e2694585ca4f1dc5f1
xcoru <pcd510@hotmail.com>
2020-07-05 20:54:47 -0500

    Update CARTERA_CLIENTES.sql

--- XRPrestamos/src/database/scripts/CARTERA_CLIENTES.sql
+++ XRPrestamos/src/database/scripts/CARTERA_CLIENTES.sql
@@ -9,12 +9,15 @@ CREATE PROCEDURE CARTERA_CLIENTES(IN usuario VARCHAR(30))
 		-- e Usuario_ruta
 		-- f Usuario
 
-		SELECT a.alias, CONCAT_WS(' ', a.nombre, a.apellido_paterno, a.apellido_materno) AS "nombre", b.monto_total, SUM(c.monto) AS "pagado", (b.monto_total - SUM(c.monto)) AS "restante" FROM credito b
+		SELECT a.alias, CONCAT_WS(' ', a.nombre, a.apellido_paterno, a.apellido_materno) AS "nombre",
+		((b.pagos_total * monto_pago) - SUM(c.monto)) AS "restante" 
+		
+		FROM credito b
 			INNER JOIN usuario_establecimiento AS d ON d.id_usuario_establecimiento = b.id_usuario_establecimiento
 			INNER JOIN persona AS a ON a.ine = d.ine
 			INNER JOIN usuario_ruta AS e ON e.id_ruta = d.id_ruta
 			INNER JOIN usuario AS f ON f.id_usuario = e.id_usuario
-			JOIN abono AS c 
+			LEFT JOIN abono AS c ON c.folio_credito = b.folio_credito
 			WHERE f.id_usuario = usuario AND b.id_estado_credito = 1
 			GROUP BY b.folio_credito;
    END //
