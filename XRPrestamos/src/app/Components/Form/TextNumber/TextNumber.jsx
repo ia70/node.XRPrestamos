54b99d0dcfff6d92f03922a73a9c1ea567a405a1
xcoru <pcd510@hotmail.com>
2020-07-09 03:20:14 -0500

    Update TextNumber.jsx

--- XRPrestamos/src/app/Components/Form/TextNumber/TextNumber.jsx
+++ XRPrestamos/src/app/Components/Form/TextNumber/TextNumber.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 // required | id | label | holder | help
 
 export function TextNumber(props) {
-    if(props.required){
+    if (props.required) {
         return (
             <div className="form-group col-xs-12 col-sm-4 col-lg-3">
                 <label htmlFor={props.id}>{props.label}</label>
-                <input type="number" className="form-control" id={props.id} aria-describedby="textHelp" placeholder={props.holder} required min={0} />
-                    <small id="textHelp" className="form-text text-muted">{props.help}</small>
+                <input type="number" className="form-control" id={props.id} aria-describedby="textHelp" placeholder={props.holder} onChangeCapture={(props.evento != null ? (e) => props.evento(document.getElementById(props.id).value) : null)} required min={0} />
+                <small id="textHelp" className="form-text text-muted">{props.help}</small>
             </div>
         );
-    }else{
+    } else {
         return (
             <div className="form-group col-xs-12 col-sm-4 col-lg-3">
                 <label htmlFor={props.id}>{props.label}</label>
-                <input type="number" className="form-control" id={props.id} aria-describedby="textHelp" placeholder={props.holder} min={0} />
-                    <small id="textHelp" className="form-text text-muted">{props.help}</small>
+                <input type="number" className="form-control" id={props.id} aria-describedby="textHelp" placeholder={props.holder} onChangeCapture={(props.evento != null ? (e) => props.evento(document.getElementById(props.id).value) : null)} min={0} />
+                <small id="textHelp" className="form-text text-muted">{props.help}</small>
             </div>
         );
     }
