1160f28820a66910f242e7feb25fdd33540c9da7
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-07-09 12:04:46 -0500

    Create nuevo_credito.routes.js

---
+++ XRPrestamos/src/routes/lib/nuevo_credito.routes.js
@@ -0,0 +1,49 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../../database');
+const { access } = require('../../lib/security');
+
+//->>>>>    AGREGAR     --------------------------------------------------------------------
+router.post('/', async (req, res) => {
+    try {
+        if (await access(req.body.hash, req.body.user)) {
+            let r_d_solicitud = false;
+
+            let rol = [req.body.rol];
+
+            let datos = [];
+
+            if (rol == 1 || rol == 4) {
+                datos = await pool.query('CALL COBRO_DIA(?)', "0");
+            } else if (rol == 2) {
+                datos = await pool.query('CALL COBRO_DIA(?)', [req.body.user]);
+            }
+
+            if (JSON.stringify(datos) != '[]') {
+                r_d_solicitud = true;
+            }
+            let respuesta = {
+                response: r_d_solicitud,
+                session: true,
+                solicitud: datos[0]
+            };
+
+            res.status(200).send(respuesta);
+
+        } else {
+            res.status(400).send({
+                session: false
+            });
+        }
+    } catch (e) {
+        res.status(400).send({
+            response: false,
+            session: true,
+            error: e
+        });
+    }
+});
+
+module.exports = router;
