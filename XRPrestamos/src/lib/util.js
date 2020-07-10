5c4a4d2ebaab8c359e96e4acdd180a52e1bcbad7
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-07-10 14:14:40 -0500

    Update util.js

--- XRPrestamos/src/lib/util.js
+++ XRPrestamos/src/lib/util.js
@@ -1,18 +1,15 @@
 const moment = require('moment');
 
 function getDateTime() {
-    var d = moment();
     return moment().format('YYYY-MM-DD hh:mm:ss');
 }
 
 function getShortDate(){
-    var d = moment();
     return moment().format('YYYY-MM-DD');
 }
 
 function getShortDateMX(){
-    var d = moment();
     return moment().format('DD-MM-YYYY');
 }
 
-module.exports = { getDateTime, getShortDate, getShortDateMX };
+module.exports = { getDateTime, getShortDate, getShortDateMX};
