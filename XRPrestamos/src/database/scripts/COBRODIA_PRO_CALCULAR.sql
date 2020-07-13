88f0f513fdd358fb58352bab08267308cc311a2d
Daniel Perez <pcd510@hotmail.com>
2020-07-13 16:07:54 -0500

    Revert "Update COBRODIA_PRO_CALCULAR.sql"
    This reverts commit 0ff9f76f00b27825a359df36cbb93979082cd2b1.

--- XRPrestamos/src/database/scripts/COBRODIA_PRO_CALCULAR.sql
+++ XRPrestamos/src/database/scripts/COBRODIA_PRO_CALCULAR.sql
@@ -10,64 +10,18 @@ BEGIN
 	DECLARE var_fecha DATE;												-- Fecha del siguiente pago
 	DECLARE var_ruta INT DEFAULT 0;								-- Ruta
 	DECLARE var_usuario VARCHAR(30) DEFAULT ""; 	-- Usuario (cobrador)
-	DECLARE var_no_abonos INT DEFAULT 0;
-	DECLARE var_fecha_abono DATE DEFAULT "";
-	DECLARE var_estadistica_existe INT DEFAULT 0;
-	
-	DECLARE var_ine VARCHAR(100) DEFAULT "";
-	DECLARE var_monto_prestamo DECIMAL(10,2) DEFAULT 0;
-	DECLARE var_fecha_entrega DATE DEFAULT "";
-	DECLARE var_no_pagos INT DEFAULT 0;
-	DECLARE var_folio_credito VARCHAR(50);
- 
+	DECLARE var_ine VARCHAR(100);
+
 	-- DECLARACION DEL CURSOR -----------------------------------------------------------------------------------------
 	DEClARE curLista 
 		CURSOR FOR 
-			SELECT a.folio_credito, a.id_tipo_cobro, a.fecha_siguiente_pago, b.id_ruta, c.id_usuario, COUNT(d.monto) AS "no_abonos" FROM credito a
+			SELECT a.folio_credito, a.id_tipo_cobro, a.fecha_siguiente_pago, b.id_ruta, c.id_usuario, b.ine FROM credito a
 				INNER JOIN usuario_establecimiento AS b ON b.id_usuario_establecimiento = a.id_usuario_establecimiento
 				INNER JOIN usuario_ruta AS c ON c.id_ruta = b.id_ruta
-				LEFT JOIN abono AS d ON d.folio_credito = a.folio_credito
 				WHERE a.fecha_siguiente_pago <= CURDATE() AND a.id_estado_credito = 1 ORDER BY a.folio_credito ASC;
-
-
-
--- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-	DEClARE curLista2 
-			CURSOR FOR 
-				SELECT a.folio_credito, d.ine, c.monto_credito, c.fecha_entrega, c.pagos_total
-				FROM cobro_dia a
-				INNER JOIN credito AS c ON c.folio_credito = a.folio_credito
-				INNER JOIN usuario_establecimiento AS d ON d.id_usuario_establecimiento = c.id_usuario_establecimiento
-				LEFT JOIN abono AS b ON b.folio_credito = a.folio_credito AND b.fecha_abono = DATE(DATE_SUB( NOW(), INTERVAL 1 DAY))
-				WHERE b.fecha_abono IS NULL;
 
 	-- DECLARACION DE ERROR PARA FOR(CURSOR) --------------------------------------------------------------------------
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-	
-	SET var_fecha_abono = (DATE_SUB( NOW(), INTERVAL 1 DAY));
-	OPEN curLista2;
-	
-	ciclo2: LOOP
-			FETCH curLista2 INTO var_folio_credito, var_ine, var_monto_prestamo, var_fecha_entrega, var_no_pagos;
-			
-			IF done THEN 
-				LEAVE ciclo2;
-			END IF;
-			
-				INSERT INTO abono VALUES(var_folio_credito, 0, 0, 4, var_fecha_abono, 1, NOW());
-				
-				SET var_estadistica_existe = (SELECT COUNT(ine) FROM estadistica_cliente WHERE folio_credito = var_folio_credito);
-				IF var_estadistica_existe > 0 THEN
-					UPDATE estadistica_cliente SET no_pagos = (no_pagos + 1) WHERE folio_credito = var_folio_credito;
-				ELSE
-					INSERT INTO estadistica_cliente VALUES(var_folio_credito, var_ine, var_monto_prestamo, var_fecha_entrega, var_no_pagos, 0, 0, 0, 1);
-				END IF;
-			
-		END LOOP ciclo2;
-		CLOSE curLista2;
-
--- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 	-- Se inicializa el cursor
 	OPEN curLista;
@@ -77,7 +31,7 @@ BEGIN
 
 	-- Se inicia ciclo repetitivo -------------------------------------------------------------------------------------
 	ciclo: LOOP
-		FETCH curLista INTO var_folio, var_id_tipo, var_fecha, var_ruta, var_usuario, var_no_abonos;
+		FETCH curLista INTO var_folio, var_id_tipo, var_fecha, var_ruta, var_usuario, var_ine;
 		
 		IF done THEN 
 			LEAVE ciclo;
