bb81079284f58082cd547228fe77f2e4717a5d9e
Daniel Perez <pcd510@hotmail.com>
2020-07-14 14:08:10 -0500

    Update COBRO_DIA_CORTE.sql

--- XRPrestamos/src/database/scripts/COBRO_DIA_CORTE.sql
+++ XRPrestamos/src/database/scripts/COBRO_DIA_CORTE.sql
@@ -29,36 +29,33 @@ BEGIN
 	
 */
 		SELECT 
-					b.id_ruta,
+					a.id_ruta,
 					b.descripcion AS "ruta_descripcion",
 					a.id_usuario,
-					SUM(c.monto) AS "total_recolectado",
+					SUM(IF(c.monto IS NULL,0, c.monto)) AS "total_recolectado",
 					SUM(d.monto_pago) AS "total_deberia_recolectar_dia",
-					SUM(IF(c.id_tipo_pago > 5, IF(c.monto < d.monto_pago, c.monto, d.monto_pago ), IF(c.id_tipo_pago != 3, c.monto, 0))) AS "total_recolectado_sin_extras",
-					SUM(IF(c.id_tipo_pago = 2, c.monto,0)) AS "monto_abonos",
-					SUM(IF(c.id_tipo_pago = 3, c.monto - d.monto_pago, 0)) AS "monto_extras",
-					SUM(IF(c.id_tipo_pago = 1, c.monto, 0)) AS "monto_normal",
-					SUM(IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago IS NULL,d.monto_pago,0))) AS "monto_no_pagos",
-					SUM(IF(c.id_tipo_pago = 2, d.monto_pago - c.monto, 0)) AS "monto_defici_abonos",
-					
-					SUM(IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago = 2, d.monto_pago - c.monto,IF(c.id_tipo_pago IS NULL,d.monto_pago,0)))) AS "monto_defici_total",
-					
-					
-					SUM(IF(c.id_tipo_pago = 7,IF(c.monto >= d.monto_pago, c.monto - d.monto_pago,c.monto),0)) AS "monto_remanente",
-					COUNT(a.folio_credito) AS "total_cli_visitar",
-					COUNT(c.id_abono) AS "total_cli_visitados",
-					SUM(IF(c.id_tipo_pago = 4, 1, IF(c.id_tipo_pago IS NULL,1,0))) AS "total_cli_no_pago",
-					SUM(IF(c.id_tipo_pago = 1, 1, 0)) AS "total_cli_pago",
-					SUM(IF(c.id_tipo_pago = 2, 1, 0)) AS "total_cli_abonaron",
-					SUM(IF(c.id_tipo_pago = 3, 1, 0)) AS "total_cli_extra",
-					(COUNT(a.id_ruta) - COUNT(c.id_abono)) AS "total_cli_no_vicitaron",
-					SUM(IF(c.id_tipo_pago > 5, 1,0)) AS "no_creditos_terminados",
-					SUM(IF(c.id_tipo_pago = 7, 1,0)) AS "no_renovaciones"
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago > 5, IF(c.monto < d.monto_pago, c.monto, d.monto_pago ), IF(c.id_tipo_pago != 3, c.monto, 0)))) AS "total_recolectado_sin_extras",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 2, c.monto,0))) AS "monto_abonos",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 3, c.monto - d.monto_pago, 0))) AS "monto_extras",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 1, c.monto, 0))) AS "monto_normal",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago IS NULL,d.monto_pago,0)))) AS "monto_no_pagos",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 2, d.monto_pago - c.monto, 0))) AS "monto_defici_abonos",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago = 2, d.monto_pago - c.monto,IF(c.id_tipo_pago IS NULL,d.monto_pago,0))))) AS "monto_defici_total",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 7, IF(c.monto >= d.monto_pago, c.monto - d.monto_pago, c.monto), 0))) AS "monto_remanente",
+					COUNT(a.folio_credito) 	AS "total_cli_visitar",
+					COUNT(c.id_abono) 			AS "total_cli_visitados",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 4, 1, IF(c.id_tipo_pago IS NULL,1,0)))) AS "total_cli_no_pago",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 1, 1, 0))) AS "total_cli_pago",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 2, 1, 0))) AS "total_cli_abonaron",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 3, 1, 0))) AS "total_cli_extra",
+					(COUNT(a.id_ruta) - COUNT(c.id_abono)) 									AS "total_cli_no_vicitaron",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago > 5, 1, 0))) AS "no_creditos_terminados",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 7, 1, 0))) AS "no_renovaciones"
 
-					FROM ruta b
-					INNER JOIN cobro_dia AS a ON a.id_ruta = b.id_ruta
+					FROM cobro_dia a
+					INNER JOIN ruta AS b ON a.id_ruta = b.id_ruta
 					INNER JOIN credito AS d ON d.folio_credito = a.folio_credito
-					LEFT JOIN abono AS c ON c.folio_credito = a.folio_credito
+					LEFT JOIN abono AS c ON c.folio_credito = a.folio_credito AND c.fecha_abono = CURDATE()
 					GROUP BY a.id_ruta;
 					
 END // 
