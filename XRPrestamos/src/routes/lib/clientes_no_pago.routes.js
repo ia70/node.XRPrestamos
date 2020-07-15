0c011e2f06263efbeb49585efc7f25d8b8193a8b
XCORU Desarrollo de Software <pcd510@hotmail.com>
2020-07-14 19:34:19 -0500

    Create clientes_no_pago.routes.js

---
+++ XRPrestamos/src/routes/lib/clientes_no_pago.routes.js
@@ -0,0 +1,54 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../../database');
+const { access } = require('../../lib/security');
+const { getShortDate } = require('../../lib/util');
+
+//->>>>>    AGREGAR     --------------------------------------------------------------------
+router.post('/', async (req, res) => {
+    try {
+        if (await access(req.body.hash, req.body.user)) {
+            let r_d_solicitud = false;
+
+            let rol = [req.body.rol];
+            let info = [req.body.info];
+            let fecha ="";
+
+            if(info.fecha == null || info.fecha == ""){
+                fecha = getShortDate();
+            }else{
+                fecha = info.fecha;
+            }
+
+            let datos = [];
+
+            datos = await pool.query('CALL CLIENTES_NO_PAGO(?)', [fecha]);
+
+            if (JSON.stringify(datos) != '[]') {
+                r_d_solicitud = true;
+            }
+            let respuesta = {
+                response: r_d_solicitud,
+                session: true,
+                solicitud: datos[0]
+            };
+
+            res.status(200).send(respuesta);
+
+        } else {
+            res.status(400).send({
+                session: false
+            });
+        }
+    } catch (e) {
+        res.status(400).send({
+            response: false,
+            session: true,
+            error: e
+        });
+    }
+});
+
+module.exports = router;
