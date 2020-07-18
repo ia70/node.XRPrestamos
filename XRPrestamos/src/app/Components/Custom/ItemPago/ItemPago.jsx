d146d5d86b0d9d3fb53b7ddc427bea51a50cfda6
Daniel Perez <pcd510@hotmail.com>
2020-07-18 01:25:42 -0500

    Update ItemPago.jsx

--- XRPrestamos/src/app/Components/Custom/ItemPago/ItemPago.jsx
+++ XRPrestamos/src/app/Components/Custom/ItemPago/ItemPago.jsx
@@ -1,22 +1,123 @@
 import React from 'react';
+import { BtnClose } from '../../Form/BtnClose/BtnClose.jsx';
+import ModalRuta from '../ModalRuta/ModalRuta.jsx';
 
+import './ItemPago.css'
+/*PROPIEDADES -----------------------------------------------------
 
-import './ItemPago.css';
-
-/* --> PROPIEDADES
-    1- id
-    2- label
+    	1 - folio_credito
+		2 - ine
+		3 - nombre (nombre completo)
+		4 - alias
+		5 - telefono
+		6 - monto_credito
+		7 - monto_total
+		8 - monto_pago
+		9 - pagado
+		10 - atrasos_no
+		11 - atrasos_monto
+		12 - extra_no
+		13 - extra_monto
+		14 - restante_no
+		15 - restante_monto
+		16 - restante_total
+        17 - abono_hoy
+        18 - stateItem            Estado (1 Aprobado, 2 En espera, 3 Rechazado )
+        19 - stateDescription     Texto de descripcion
+        20 - amountDescription    Descripción del monto
+        21 - amount               Monto $
+        22 - close                Mostrar boton cerrar (true, false)
+        23 - modal               (true | false) Especifica si debe mostrar ventana modal
 
 */
 export function ItemPago(props) {
+    let btncerrar = <BtnClose label="X" />;
+    let class_br = "";
+    let class_bl = "";
+    let texto = "";
+    let mtarget = "";
+    let mtoggle = "";
+
+    let _evento = [];
+
+    if (props.evento != null) {
+        _evento = props.evento;
+    }
+
+    if (props.modal != null) {
+        if (props.modal) {
+            mtarget = "#item" + props.number;
+            mtoggle = "modal";
+        }
+    }
+
+    if (!props.close) {
+        btncerrar = "";
+    }
+
+    let porcentaje = (100 / props.info.total_deberia_recolectar_dia) * props.info.total_recolectado_sin_extras;
+    let estado = 0;
+    console.log(porcentaje);
+
+    if(porcentaje < 65) {
+        estado = 4;
+        texto = "Mal";
+    }else if(porcentaje <= 78){
+        estado = 2;
+        texto = "Regular";
+    }else if(porcentaje <= 90){
+        estado = 1;
+        texto = "Bien";
+    }else{
+        estado = 5;
+        texto = "Muy bien";
+    }
+
+
+    if (texto == "") {
+        class_br = "stateitem_border_br";
+        class_bl = "stateitem_border_bl";
+    }
+
     return (
-        <div className="row px-3 itempago m-0">
-            <div className="col-8">
-                <h6>{props.concepto}</h6>
+        <div className="col-xs-12 col-sm-6 col-lg-4 col-xl-3 mb-1">
+            <div className="col-12 mb-2" data-toggle={mtoggle} data-target={mtarget}>
+                <div className={"main container-fluid stateitem_border_br stateitem_border_bl stateitem_border_tr stateitem_border_tl stateitem_state_" + estado}>
+                    <div className="row">
+                        <div className={"col-xs-12 col-auto stateitem_num stateitem_center stateitem_border_tl " + class_bl}>
+                            <h2>{props.number}</h2>
+                        </div>
+                        <div className="col col-xs-12">
+                            <div className="row stateitem_alias stateitem_center stateitem_border_tr">
+                                <div className="col col-xs-12 stateitem_center">
+                                    {props.info.ruta_descripcion}
+                                </div>
+                            </div>
+                            <div className="row stateitem stateitem_name stateitem_center">
+                                {props.info.id_usuario}
+                            </div>
+                            <div className={"row align-self-stretch stateitem_center " + class_br}>
+                                <div className="col col-xs-12 stateitem_state_amount_description">
+                                    {"Recolectado: "}
+                                </div>
+                                <div className={"col-auto col-xs-12 align-self-stretch align-items-center stateitem_state_amount " + class_br}>
+                                    {props.info.total_recolectado}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row stateitem stateitem_center stateitem_border_br stateitem_border_bl">
+                        {texto}
+                    </div>
+                </div>
             </div>
-            <div className="col-4">
-                <h6>{props.monto}</h6>
-            </div>
+
+            <ModalRuta
+                id={"item" + props.number}
+                number={props.number}
+                info={props.info}
+                evento={_evento}
+            />
         </div>
     );
 }
