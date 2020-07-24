83fee83104aa84f0bf075b3c77e2ac6f9565d943
Daniel Perez <pcd510@hotmail.com>
2020-07-23 23:44:18 -0500

    Update COBRO_DIA_CORTE.sql

--- XRPrestamos/src/database/scripts/COBRO_DIA_CORTE.sql
+++ XRPrestamos/src/database/scripts/COBRO_DIA_CORTE.sql
@@ -32,7 +32,7 @@ BEGIN
 					a.id_ruta,
 					b.descripcion AS "ruta_descripcion",
 					a.id_usuario,
-					SUM(IF(c.monto IS NULL,0, c.monto)) AS "total_recolectado",
+					SUM(IF(c.monto IS NULL,1, c.monto)) AS "total_recolectado",
 					SUM(d.monto_pago) AS "total_deberia_recolectar_dia",
 				
 					SUM(IF(c.monto IS NULL, 0, IF(c.monto < d.monto_pago, c.monto, d.monto_pago ))) AS "total_recolectado_sin_extras",
@@ -40,18 +40,18 @@ BEGIN
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 2, c.monto,0))) AS "monto_abonos",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 3, c.monto - d.monto_pago, 0))) AS "monto_extras",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 1, c.monto, 0))) AS "monto_normal",
-					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago IS NULL,d.monto_pago,0)))) AS "monto_no_pagos",
+					SUM(IF(c.monto IS NULL,d.monto_pago,IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago IS NULL,d.monto_pago,0)))) AS "monto_no_pagos",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 2, d.monto_pago - c.monto, 0))) AS "monto_defici_abonos",
-					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago = 2, d.monto_pago - c.monto,IF(c.id_tipo_pago IS NULL,d.monto_pago,0))))) AS "monto_defici_total",
+					SUM(IF(c.monto IS NULL,d.monto_pago,IF(c.id_tipo_pago = 4, d.monto_pago, IF(c.id_tipo_pago = 2, d.monto_pago - c.monto,IF(c.id_tipo_pago IS NULL,d.monto_pago,0))))) AS "monto_defici_total",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 7, IF(c.monto >= d.monto_pago, c.monto - d.monto_pago, c.monto), 0))) AS "monto_remanente",
 					COUNT(a.folio_credito) 	AS "total_cli_visitar",
-					COUNT(c.id_abono) 			AS "total_cli_visitados",
-					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 4, 1, IF(c.id_tipo_pago IS NULL,1,0)))) AS "total_cli_no_pago",
+					COUNT(IF(c.id_abono IS NULL, 0,1)) 			AS "total_cli_visitados",
+					SUM(IF(c.monto IS NULL,1,IF(c.id_tipo_pago = 4, 1, IF(c.id_tipo_pago IS NULL,1,0)))) AS "total_cli_no_pago",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 1, 1, 0))) AS "total_cli_pago",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 2, 1, 0))) AS "total_cli_abonaron",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 3, 1, 0))) AS "total_cli_extra",
 					(COUNT(a.id_ruta) - COUNT(c.id_abono)) 									AS "total_cli_no_vicitaron",
-					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago > 5, 1, 0))) AS "no_creditos_terminados",
+					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago >= 6, 1, 0))) AS "no_creditos_terminados",
 					SUM(IF(c.monto IS NULL,0,IF(c.id_tipo_pago = 7, 1, 0))) AS "no_renovaciones"
 
 					FROM cobro_dia a
@@ -62,3 +62,28 @@ BEGIN
 					
 END // 
 DELIMITER;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
