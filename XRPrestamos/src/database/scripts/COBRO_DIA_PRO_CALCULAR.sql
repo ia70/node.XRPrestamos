16e50237533bb7841dd0deb76279743afb6cdb2d
Daniel Perez <pcd510@hotmail.com>
2020-07-23 23:44:23 -0500

    Update COBRO_DIA_PRO_CALCULAR.sql

--- XRPrestamos/src/database/scripts/COBRO_DIA_PRO_CALCULAR.sql
+++ XRPrestamos/src/database/scripts/COBRO_DIA_PRO_CALCULAR.sql
@@ -11,14 +11,16 @@ BEGIN
 	DECLARE var_ruta INT DEFAULT 0;								-- Ruta
 	DECLARE var_usuario VARCHAR(30) DEFAULT ""; 	-- Usuario (cobrador)
 	DECLARE var_ine VARCHAR(100);
+	DECLARE var_id_estado INT DEFAULT 0;
+	DECLARE var_id_estado_credito INT DEFAULT 0;
 
 	-- DECLARACION DEL CURSOR -----------------------------------------------------------------------------------------
 	DEClARE curLista 
 		CURSOR FOR 
-			SELECT a.folio_credito, a.id_tipo_cobro, a.fecha_siguiente_pago, b.id_ruta, c.id_usuario, b.ine FROM credito a
+			SELECT a.folio_credito, a.id_tipo_cobro, a.fecha_siguiente_pago, b.id_ruta, c.id_usuario, b.ine, a.id_estado, a.id_estado_credito FROM credito a
 				INNER JOIN usuario_establecimiento AS b ON b.id_usuario_establecimiento = a.id_usuario_establecimiento
 				INNER JOIN usuario_ruta AS c ON c.id_ruta = b.id_ruta
-				WHERE a.fecha_siguiente_pago <= CURDATE() AND a.id_estado_credito = 1 ORDER BY a.folio_credito ASC;
+				WHERE (a.fecha_siguiente_pago <= CURDATE() AND a.id_estado_credito = 1) OR (a.fecha_siguiente_pago = CURDATE() AND a.id_estado_credito>2)  ORDER BY a.folio_credito ASC;
 
 	-- DECLARACION DE ERROR PARA FOR(CURSOR) --------------------------------------------------------------------------
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
@@ -31,13 +33,13 @@ BEGIN
 
 	-- Se inicia ciclo repetitivo -------------------------------------------------------------------------------------
 	ciclo: LOOP
-		FETCH curLista INTO var_folio, var_id_tipo, var_fecha, var_ruta, var_usuario, var_ine;
+		FETCH curLista INTO var_folio, var_id_tipo, var_fecha, var_ruta, var_usuario, var_ine, var_id_estado, var_id_estado_credito;
 		
 		IF done THEN 
 			LEAVE ciclo;
 		END IF;
 		
-		IF var_fecha < CURDATE() THEN
+		IF var_fecha < CURDATE() AND var_id_estado = 1 THEN
 			SET var_fecha = COBRO_DIA_CAL_FECHA_PAGO(var_fecha, var_id_tipo);
 			UPDATE credito SET fecha_siguiente_pago = var_fecha WHERE folio_credito = var_folio;
 		END IF;
